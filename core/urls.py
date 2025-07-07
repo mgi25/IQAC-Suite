a3df790eb167b8cdfb897981671b75bf26acaa7a
ANISHPILLAI07 <agirish700@gmail.com>
2025-07-07 12:46:04 +0530

    CDL dashboard to main homepage

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('',            views.dashboard,       name='dashboard'),
     path('dashboard/',  views.admin_dashboard, name='admin_dashboard'),
 
+    # CDL Dashboard
+    path('cdl/', views.cdl_dashboard, name='cdl_dashboard'),
+
     # Event Proposals
     path('propose-event/', views.propose_event, name='propose_event'),
     path('proposal-status/<int:pk>/', views.proposal_status, name='proposal_status'),
