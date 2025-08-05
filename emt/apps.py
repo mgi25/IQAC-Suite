7614f642856b33069da37b65ff70ec7c18e750b7
Abhinay <sahuabhinay317@gmail.com>
2025-08-05 14:49:09 +0530

    logs for report and proposal

--- emt/apps.py
+++ emt/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
-
 class EmtConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'emt'
+    
+    def ready(self):
+        """
+        Import signals when the app is ready.
+        """
+        import emt.signals
