b02efc7f51c807b7a25a6b50d23c63ee3dac7a15
Alen Jin Shibu <153077870+mgi25@users.noreply.github.com>
2025-08-13 11:02:09 +0530

    Restrict SDG goals to UN list

---
+++ core/migrations/0033_seed_sdg_goals.py
@@ -0,0 +1,24 @@
+from django.db import migrations
+from core.models import SDG_GOALS
+
+
+def seed_sdg_goals(apps, schema_editor):
+    SDGGoal = apps.get_model('core', 'SDGGoal')
+    for name in SDG_GOALS:
+        SDGGoal.objects.get_or_create(name=name)
+    SDGGoal.objects.exclude(name__in=SDG_GOALS).delete()
+
+
+def reverse_seed(apps, schema_editor):
+    SDGGoal = apps.get_model('core', 'SDGGoal')
+    SDGGoal.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0032_popsochangenotification_popsoassignment"),
+    ]
+
+    operations = [
+        migrations.RunPython(seed_sdg_goals, reverse_seed),
+    ]
