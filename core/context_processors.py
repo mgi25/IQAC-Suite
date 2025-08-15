ab4ca2f32ce63b1ac60a43f51bfd6ce90425dccc
Alen Jin Shibu <153077870+mgi25@users.noreply.github.com>
2025-08-15 13:44:53 +0530

    Fix header notifications and improve styling

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,4 @@
 from django.utils import timezone
-from django.utils.timesince import timesince
 from django.db.models import Q
 from datetime import timedelta
 from emt.models import EventProposal
@@ -23,16 +22,25 @@ def notifications(request):
 
     notif_list = []
     for p in proposals:
-        # Determine notification type based on status
+        """Build notification payloads compatible with the header dropdown."""
         if p.status == EventProposal.Status.REJECTED:
             n_type = 'alert'
+            icon = 'triangle-exclamation'
         elif p.status in [EventProposal.Status.SUBMITTED, EventProposal.Status.UNDER_REVIEW]:
             n_type = 'reminder'
+            icon = 'clock'
         else:
             n_type = 'info'
-        message = f"{p.event_title or 'Event Proposal'} - {p.get_status_display()}"
-        time_label = timesince(p.updated_at, timezone.now()) + ' ago'
-        notif_list.append({'type': n_type, 'msg': message, 'time': time_label})
+            icon = 'circle-info'
+
+        notif_list.append({
+            'title': p.event_title or 'Event Proposal',
+            'message': p.get_status_display(),
+            'created_at': p.updated_at,
+            'icon': icon,
+            'type': n_type,
+            'is_read': False,
+        })
 
     return {'notifications': notif_list}
 
