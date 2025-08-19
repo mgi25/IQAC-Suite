5f6b5eba4c23dd4ed9f5cc7425247c17684c89a2
MGI <alen.shibu@bscdsh.christuniversity.in>
2025-08-19 11:47:53 +0530

    FIx

--- iqac_project/settings.py
+++ iqac_project/settings.py
@@ -164,7 +164,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID = int(os.getenv("SITE_ID", "2"))
+SITE_ID = 2
 
 
 LOGIN_URL = '/accounts/login/'
